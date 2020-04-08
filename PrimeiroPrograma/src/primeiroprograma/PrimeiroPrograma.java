63bb4a8a91a24daceece8baa11279e6f2b69066c
chmendonca <chmendonca1977@gmail.com>
2020-04-08 19:15:09 -0300

    primeiro programa
    Primeiro programa em java (Hello, World!) com alguns comentários

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package primeiroprograma;
+
+/**
+ *
+ * @author Cassio
+ */
+public class PrimeiroPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    //psvm
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //sout
+        System.out.print("Olá, Mundo!\n"); //print string não pula linha
+        System.out.println("Hello, World!"); //print ln pula linha após o print
+    }
+    
+}
