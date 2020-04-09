e18e7af8dbe122af5a1ca017513bd26f2e33dc30
chmendonca <chmendonca1977@gmail.com>
2020-04-09 17:52:36 -0300

    ResoluçãoDeTela
    This module gets the screen resolution (size) in pixels

---
+++ ResolucaoDaTela/src/resolucaodatela/ResolucaoDaTela.java
@@ -0,0 +1,34 @@
+/*
+ * Class: 4 (exercises)
+ * Second homework
+ * Objective: To create a code to get the screen resolution
+ * @author Cassio - 09Apr2020
+ * help: http://alvinalexander.com/blog/post/jfc-swing/how-determine-get-screen-size-java-swing-app/
+ */
+package resolucaodatela;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class ResolucaoDaTela {
+    public static void main(String[] args) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        //System.out.println(screenSize.getHeight());
+        //System.out.println(screenSize.getWidth());
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        //System.out.println(screenHeight);
+        //System.out.println(screenWidth);
+        
+        System.out.print("Sua tela tem resolução ");
+        System.out.print(screenHeight);
+        System.out.print(" x ");
+        System.out.println(screenWidth);
+        
+        
+        
+               
+        
+    }
+    
+}
