9dcb9844045e6f6330c75614bcece55bd2a336b5
chmendonca <chmendonca1977@gmail.com>
2020-04-09 17:40:29 -0300

    Update PrimeiroPrograma.java
    Added a header, removed some default comments and added a few comments to track the quick commands hints

--- PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -1,20 +1,18 @@
+/*
+ * Class: 4
+ * Class example
+ * Objective: To create a code to print the 'Hello, World!'
+ * @author Cassio - 08Apr2020
+ * help: 
+ */
+
 package primeiroprograma;
 
-/**
- *
- * @author Cassio
- */
 public class PrimeiroPrograma {
-
-    /**
-     * @param args the command line arguments
-     */
-    //psvm
-    public static void main(String[] args) {
+    public static void main(String[] args) { //psvm
         // TODO code application logic here
-        //sout
-        System.out.print("Olá, Mundo!\n"); //print string não pula linha
-        System.out.println("Hello, World!"); //print ln pula linha após o print
+        System.out.print("Olá, Mundo!\n"); //print string não pula linha //sout
+        System.out.println("Hello, World!"); //print ln pula linha após o print //sout
     }
     
 }
