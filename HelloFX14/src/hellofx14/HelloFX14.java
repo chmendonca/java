d8228139c0d50cf5e8b9775d1bcc8893f7312f6a
chmendonca <chmendonca1977@gmail.com>
2020-04-14 18:04:52 -0300

    HelloFX14
    This is the example created based on the youtube video to create JavaFX file in the SDK14 and OpenFX14.
    https://www.youtube.com/watch?v=BMUOp6gl_nE&t=6s
    https://www.youtube.com/watch?v=IdTc_ZIGlMg&t=5s

---
+++ HelloFX14/src/hellofx14/HelloFX14.java
@@ -0,0 +1,23 @@
+
+package hellofx14;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloFX14 extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("HelloFXML14.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Hello World!");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
